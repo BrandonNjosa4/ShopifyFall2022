2fe421e7c0b02405f9bea83c75e10a9e20e8ff07
BrandonNjosa4 <bnjosa@gmail.com>
2022-05-17 17:38:58 -0400

    first commit

--- server/index.js
+++ server/index.js
@@ -31,10 +31,10 @@ app.use('/location', authenticateUser, locations)
 // deployment
 __dirname=path.resolve()
 if(process.env.NODE_ENV==='production') {
-app.use(express.static(path.join(__dirname,'..','client/build')));
+app.use(express.static(path.join(__dirname,'client/build')));
 
 app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname,'..','client','build','index.html'))
+  res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 })
 }
 
